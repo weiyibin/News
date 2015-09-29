0d8f4d20a469fe730f0f501427074a94ff95c036
weiyibin <568797341@qq.com>
2015-09-29 17:15:57 +0800

    修改

---
+++ New/New/Classes/News/CollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.h
+//  New
+//
+//  Created by 危一斌 on 15/9/29.
+//  Copyright (c) 2015年 危一斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@end
--- New/New/Classes/News/CollectionViewCell.m
+++ New/New/Classes/News/CollectionViewCell.m
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  CollectionViewCell.m
 //  New
 //
 //  Created by 危一斌 on 15/9/29.
 //  Copyright (c) 2015年 危一斌. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "CollectionViewCell.h"
 
-@interface ViewController : UIViewController
-
+@implementation CollectionViewCell
 
 @end
-
