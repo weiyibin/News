7fcff43eece88ccf68af7815b93de2e6bfbe5b61
weiyibin <568797341@qq.com>
2015-09-29 17:10:12 +0800

    新建项目

---
+++ New/New/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  New
+//
+//  Created by 危一斌 on 15/9/29.
+//  Copyright (c) 2015年 危一斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ New/New/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  New
+//
+//  Created by 危一斌 on 15/9/29.
+//  Copyright (c) 2015年 危一斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
